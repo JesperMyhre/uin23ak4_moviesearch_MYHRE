6f056406889221cc285b9c27f4fff2deaefa81a0
JesperMyhre <111729406+JesperMyhre@users.noreply.github.com>
2024-03-19 13:00:45 +0100

    satt opp litt greier

---
+++ src/components/BookCard.jsx
@@ -0,0 +1,14 @@
+import SearchResults from "SearchResults.jsx";
+
+export default function BookCards() {
+  return (
+    <>
+      <article>
+        <img src="#"></img>
+        <h3>TITTEL</h3>
+        <p>INFO OM FILMEN</p>
+        <button>Mer om filmen</button>
+      </article>
+    </>
+  );
+}
---
+++ src/components/Layout.jsx
@@ -0,0 +1,5 @@
+import BookCards from "./BookCard";
+
+export default function Layout() {
+  return <></>;
+}
