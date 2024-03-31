3a6abb58a1bf0137e06cec1f748708e647e29315
JesperMyhre <jesper.ovreness@gmail.com>
2024-03-31 15:27:56 +0200

    mye funksjon ferdig

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,13 @@ import SearchResult from "./components/SearchResult";
 
 export default function App() {
   const [content, setContent] = useState([]);
-  const [query, setQuery] = useState("Lord of the rings");
-  const [coverID, setCoverID] = useState("Harry Potter");
+  const [query, setQuery] = useState("Harry Potter");
 
   const [search, setSearch] = useState("");
   const handleSubmit = (e) => {
     e.preventDefault();
     setQuery(search);
-    console.log("Searching for:", search);
+    console.log("Searching for...", search);
   };
   const handleChange = (e) => {
     setSearch(e.target.value);
@@ -29,21 +28,8 @@ export default function App() {
     }
   };
 
-  const getCoverID = async (OLID) => {
-    try {
-      const response = await fetch(
-        `https://covers.openlibrary.org/b/${OLID}/${coverID}-S.jpg`
-      );
-      const data = await response.json();
-      setCoverData(data);
-    } catch {
-      console.error("Kunne ikke hente cover");
-    }
-  };
-
   useEffect(() => {
     getData();
-    getCoverID();
   }, [query]);
 
   return (
@@ -61,12 +47,29 @@ export default function App() {
       <section className="bookSection">
         {content.map((item) => (
           <article className="bookCard" key={item.key}>
-            <img src="https://placehold.co/100x150"></img>
+            <img
+              src={`https://covers.openlibrary.org/b/OLID/${item.cover_edition_key}-M.jpg`}
+            ></img>
             <span className="bookInfo">
               <h3>{item.title}</h3>
               <p>{item.author_name}</p>
               <p>Release year: {item.first_publish_year}</p>
-              <button>Mer om filmen</button>
+              <p>Rating: {item.ratings_average}</p>
+              {item.id_amazon && item.id_amazon.length >= 2 && (
+                /*knappen åpna bare til amazon landing page. ChatGPT belærte meg om hvordan form submits
+                kan bli blokkert pga js events og valideringer, samtidig som hen spytta ut det her*/
+                <form
+                  action={`https://www.amazon.com/s`}
+                  target="_blank"
+                  onSubmit={(e) => {
+                    e.preventDefault();
+                    const searchQuery = encodeURIComponent(item.id_amazon[3]);
+                    window.open(`https://www.amazon.com/s?k=${searchQuery}`);
+                  }}
+                >
+                  <button type="submit">Mer om filmen</button>
+                </form>
+              )}
             </span>
           </article>
         ))}
